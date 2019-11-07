525d12ffac9c3690dfeca388a6f09f012e96b7b7
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-11-07 08:33:38 -0800

    h

--- BankClient.java
+++ BankClient.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class BankClient{
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Create an account:");
     Bank acc = new Bank(96024, "Nick", "emailmama@hotmail.com", "joferplat3", 6.20);
     acc.getAcc();
     acc.deposit(5.89);
