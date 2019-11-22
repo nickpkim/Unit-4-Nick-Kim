49cbf2cb269ed9ed93e1954becc4887052f4a424
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-11-22 09:19:03 -0800

    hello

---
+++ ATMGUI.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ATMGUI extends Application {
+    public void start(Stage stage) {
+        Scene scene = new Scene(new ATMPane(), 600, 600);
+
+        stage.setTitle("ATM");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[]args) {
+        launch(args);
+    }
+}
+
+// git clone (url)
