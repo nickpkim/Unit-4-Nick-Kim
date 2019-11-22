d8820ac1a1626094af4ade9dcdf3ecd4d28b4a4c
Nick Kim <kimn@wlhs.wlwv.k12.or.us>
2019-11-22 08:47:31 -0800

    atm and bank acc

---
+++ ATM.java
@@ -0,0 +1,13 @@
+import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+
+public class ATM extends GridPane {
+
+}
